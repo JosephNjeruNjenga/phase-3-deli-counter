7d2dc26943a1c76966903697067b85a84f3a8c36
Joseph Njenga <joseph.njenga@student.moringaschool.com>
2023-05-24 17:07:14 +0300

    Completed assignment

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,22 @@
 # Write your code here.
-def line array
-    if array.length == 0
-        puts "The line is currently empty."
-    else
-        puts "The line is currently: #{array.map.with_index{|name, index| "#{index+1}. #{name}"}.join(" ")}"
-    end
+def line(array)
+  if array.empty?
+    puts 'The line is currently empty.'
+  else
+    puts "The line is currently: #{array.map.with_index(1) { |name, index| "#{index}. #{name}" }.join(' ')}"
+  end
 end
 
-def take_a_number (array, name)
-    array << name
-    puts "Welcome, #{name}. You are number #{array.length} in line."
+def take_a_number(array, name)
+  array << name
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
-def now_serving array
-    puts array.length == 0 ? "There is nobody waiting to be served!" : "Currently serving #{array[0]}."
-    array = array.shift
+def now_serving(array)
+  if array.empty?
+    puts 'There is nobody waiting to be served!'
+  else
+    currently_serving = array.shift
+    puts "Currently serving #{currently_serving}."
+  end
 end
